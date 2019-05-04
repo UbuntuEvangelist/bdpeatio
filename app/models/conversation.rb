37b5536fcbd8645b844cfa492d4396f7e2baf3ea
Rabbi <zamir@gmail.com>
2019-05-04 20:53:14 +0000

    first commit

---
+++ app/models/conversation.rb
@@ -0,0 +1,12 @@
+class Conversation < ActiveRecord::Base
+end
+
+# == Schema Information
+# Schema version: 20190504170028
+#
+# Table name: conversations
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
